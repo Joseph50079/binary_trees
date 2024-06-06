cc65f67eec7b34753a9942292381e00e25660b48
Joseph50079 <josephogbah2019@gmail.com>
2024-06-07 00:15:16 +0100

    4-done

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ * 
+ * 
+*/
+
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (!node)
+    {
+        return (0);
+    }
+    else if (node->left != NULL || node->right != NULL)
+    {
+        return (0);
+    }
+    else
+        return (1);
+}
